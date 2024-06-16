06a68ff27d515544957223eaee93efa09ed7a23f
JanHolger <jan@bebendorf.eu>
2024-06-16 17:10:23 +0200

    Fix endless recursion in ResourceMapper

--- src/main/java/com/lumaserv/proxmox/ve/mapper/ResourceMapper.java
+++ src/main/java/com/lumaserv/proxmox/ve/mapper/ResourceMapper.java
@@ -5,10 +5,12 @@ import com.lumaserv.proxmox.ve.model.resource.QemuVMResource;
 import com.lumaserv.proxmox.ve.model.resource.Resource;
 import com.lumaserv.proxmox.ve.model.resource.UnknownResource;
 import org.javawebstack.abstractdata.AbstractElement;
+import org.javawebstack.abstractdata.mapper.Mapper;
 import org.javawebstack.abstractdata.mapper.MapperContext;
 import org.javawebstack.abstractdata.mapper.MapperTypeAdapter;
 import org.javawebstack.abstractdata.mapper.exception.MapperException;
 import org.javawebstack.abstractdata.mapper.exception.MapperWrongTypeException;
+import org.javawebstack.abstractdata.mapper.naming.NamingPolicy;
 import org.javawebstack.abstractdata.util.Helpers;
 
 public class ResourceMapper implements MapperTypeAdapter {
@@ -24,13 +26,15 @@ public class ResourceMapper implements MapperTypeAdapter {
             throw new MapperWrongTypeException(mapperContext.getField().getName(), "object", Helpers.typeName(element));
         if(!element.object().has("type") || !element.object().get("type").isString())
             throw new MapperException("Missing or invalid resource type");
+        // We need to create a new mapper here so we don't call our own mapper recursively
+        Mapper mapper = new Mapper().namingPolicy(NamingPolicy.SNAKE_CASE);
         switch (element.object().string("type")) {
             case "qemu":
-                return mapperContext.getMapper().map(element, QemuVMResource.class);
+                return mapper.map(element, QemuVMResource.class);
             case "node":
-                return mapperContext.getMapper().map(element, NodeResource.class);
+                return mapper.map(element, NodeResource.class);
             default:
-                return mapperContext.getMapper().map(element, UnknownResource.class);
+                return mapper.map(element, UnknownResource.class);
         }
     }
 
